5bef3a52cf55e552105aaa6927ed19757f425ea2
mohamedali170 <78783747+mohamedali170@users.noreply.github.com>
2022-03-05 15:18:11 +0200

    Update url regex

--- lib/src/utils/regex.dart
+++ lib/src/utils/regex.dart
@@ -1,6 +1,6 @@
 import 'package:linkfy_text/src/enum.dart';
 
-String urlRegExp = r'(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+';
+String urlRegExp = r'[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)';
 
 String hashtagRegExp = r'(#+[a-zA-Z0-9(_)]{1,})';
 
